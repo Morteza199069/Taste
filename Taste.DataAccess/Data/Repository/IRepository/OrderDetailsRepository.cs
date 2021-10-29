96b4816b5cd1375aff659ab2c1b1dc54d3ed0b3d
Morteza Keivani <mortezakeivani.69@gmail.com>
2021-10-29 13:43:54 +0330

    Order And Cart

---
+++ Taste.DataAccess/Data/Repository/IRepository/OrderDetailsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public class OrderDetailsRepository : Repository<OrderDetails>, IOrderDetailsRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderDetailsRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderDetails orderDetails)
+        {
+            var orderDetailsFromDb = _db.OrderDetail.FirstOrDefault(m => m.Id == orderDetails.Id);
+            _db.OrderDetail.Update(orderDetailsFromDb);
+            _db.SaveChanges();
+        }
+    }
+}
